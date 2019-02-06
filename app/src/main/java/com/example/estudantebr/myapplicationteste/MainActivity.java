3d33ca1a6b247c0ca9c7f9a3e1355199fe3b0a29
Roberto Linces <Estudantebr7@gmail.com>
2019-02-06 10:50:20 -0200

    Criação da Branch Dev e navegação entre Activity 1 e Activity 2.

--- app/src/main/java/com/example/estudantebr/myapplicationteste/MainActivity.java
+++ app/src/main/java/com/example/estudantebr/myapplicationteste/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.estudantebr.myapplicationteste;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+   private Button botaoTela2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoTela2 = findViewById(R.id.botaoTela2Id);
+
+        botaoTela2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this,SegundoActivity.class));
+            }
+        });
     }
 }
