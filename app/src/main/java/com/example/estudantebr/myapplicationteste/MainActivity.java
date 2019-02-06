818a3a57a0a6d8abd568f672fe35b115cbbef024
Pedro <pedrohdsl@gmail.com>
2019-02-06 20:45:52 -0200

    Segundo commit - toast

--- app/src/main/java/com/example/estudantebr/myapplicationteste/MainActivity.java
+++ app/src/main/java/com/example/estudantebr/myapplicationteste/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                Toast.makeText(MainActivity.this, "Abrir segunda activity", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(MainActivity.this,SegundoActivity.class));
             }
         });
